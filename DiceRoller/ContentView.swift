e3417f19e450fc7f379dc6687ad8bd3e2c554741
Philipp Sanktjohanser <venture@mein.gmx>
2023-01-28 06:24:40 +0100

    Initial Commit

---
+++ DiceRoller/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DiceRoller
+//
+//  Created by Philipp Sanktjohanser on 28.01.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Philipp Sanktjohanser on 28.01.23.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
